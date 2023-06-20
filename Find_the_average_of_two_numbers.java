c260914a0d713c99521090fc6720e6232e25ad15
22A91A04M7 <119720631+22A91A04M7@users.noreply.github.com>
2023-06-20 06:01:48 +0530

    import java.util.*; public class harini {     public static void main(String args[])     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         System.out.format("%.4f",(a+b)/2.0);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class harini
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt(); 
+        System.out.format("%.4f",(a+b)/2.0);
+    }
+}
